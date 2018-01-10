713819957f02dd982132fca88554711cf6bf71c5
gonzo <micro@ralf-hochhausen.de>
2018-01-10 09:38:36 +0100

    Corrected TC severtiy and typo

--- components/Soundex/test/SoundexTest.cpp
+++ components/Soundex/test/SoundexTest.cpp
@@ -16,11 +16,11 @@ TEST_F(SoundexEncoding, PadsWithZerosToEnsureThreeDigits) {
 	ASSERT_THAT(soundex.encode("I"), Eq("I000"));
 }
 
-TEST_F(SoundexEncoding, ReplacesConsonantsWithApprpriateDigits) {
+TEST_F(SoundexEncoding, ReplacesConsonantsWithAppropriateDigits) {
 	EXPECT_THAT(soundex.encode("Ab"), Eq("A100"));
 	EXPECT_THAT(soundex.encode("Ac"), Eq("A200"));
 	EXPECT_THAT(soundex.encode("Ad"), Eq("A300"));
-	EXPECT_THAT(soundex.encode("Ax"), Eq("A200"));
+	ASSERT_THAT(soundex.encode("Ax"), Eq("A200"));
 }
 
 TEST_F(SoundexEncoding, IgnoresNoneAlphabetics) {
